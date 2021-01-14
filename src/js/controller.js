2b009576693b466f8174c99ed58f102de11f6a0c
christiesunnie <christiesunnie@gmail.com>
2021-01-14 18:43:24 -0500

    Cleanned up the controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -10,10 +10,6 @@ import addRecipeView from './views/addRecipeView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-// if (module.hot) {
-//   module.hot.accept();
-// }
-
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
@@ -72,7 +68,6 @@ const controlServings = function (newServings) {
   model.updateServing(newServings);
 
   // Update the recipe view
-  // recipeView.render(model.state.recipe);
   recipeView.update(model.state.recipe);
 };
 
