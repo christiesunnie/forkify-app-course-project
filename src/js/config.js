bf71335ac65c3fbb3e660c9230e7ce0f7443cf1d
christiesunnie <christiesunnie@gmail.com>
2021-01-14 15:39:02 -0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '4ba1ae67-3e8d-423e-952f-8019ea3c6007';
+export const MODAL_CLOSE_SEC = 2.5;
